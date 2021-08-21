58f2f096bfca25f0ec5aa9fba2ea17e1f5b731d8
Joao Vitor <jv.galvao14@gmail.com>
2021-08-21 14:18:55 -0300

    Added a 'Delete note' route, with Async functions (they rule)

--- src/models/noteModel.js
+++ src/models/noteModel.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const noteSchema = new Schema({
-    title: String,
-    body: String,
-    id: String,
-});
+const noteSchema = new Schema(
+    {
+        title: String,
+        body: String,
+        id: String,
+    },
+    {
+        timestamps: true,
+    }
+);
 
 const Note = mongoose.model("Note", noteSchema);
 
--- src/routes/Note.js
+++ src/routes/Note.js
@@ -5,6 +5,7 @@ let Note = require("../models/noteModel");
 
 router.get;
 
+//Returns the whole "Note" collection.
 router.get("/", (req, res) => {
     async function findNote() {
         try {
@@ -18,15 +19,17 @@ router.get("/", (req, res) => {
     findNote();
 });
 
+//Returns especific "Note" with the ID passed through headers.
 router.get("/find", (req, res) => {
     let id = req.headers.id;
-    async function auth() {
+    async function findNote() {
         const doc = await Note.findOne({ id: id }).exec();
         return res.json(doc);
     }
-    auth();
+    findNote();
 });
 
+//Creates a "Note" and saves it to the DB Collection.
 router.post("/", (req, res) => {
     const title = req.headers.title;
     const body = req.headers.body;
@@ -47,6 +50,20 @@ router.post("/", (req, res) => {
         }
     }
     createNewNote();
+});
+
+//deletes a Note.
+router.delete("/", (req, res) => {
+    const noteId = req.headers.id;
+    async function deleteNote() {
+        try {
+            await Note.deleteOne({ id: noteId }).exec();
+            return res.status(201).json("Note deleted");
+        } catch (error) {
+            return res.status(400).json(error);
+        }
+    }
+    deleteNote();
 });
 
 module.exports = router;
