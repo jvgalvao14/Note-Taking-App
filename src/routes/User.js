f6cb738472dd5c7faf89d902b29f224f24ce67e0
Joao Vitor <jv.galvao14@gmail.com>
2021-08-26 17:54:41 -0300

    Sucessfully creating new users on the DB

--- src/routes/User.js
+++ src/routes/User.js
@@ -2,10 +2,26 @@ const express = require("express");
 const router = express.Router();
 let User = require("../models/userModel");
 
+const bcrypt = require("bcryptjs");
+
 const urlencodedParser = express.urlencoded({ extended: true });
 
-router.post("/create", urlencodedParser, (req, res) => {
-    res.send("boa");
+router.post("/create", urlencodedParser, async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt();
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPass,
+        });
+
+        await newUser.save();
+        return res.status(201).json("Note taken!");
+    } catch (error) {
+        res.status(400).json("Error" + error);
+    }
 });
 
 router.get("/signup", (req, res) => {
