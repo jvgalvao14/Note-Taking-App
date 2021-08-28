b121134fcf746b26f45929be808145f8ce9d8e8c
Joao Vitor <jv.galvao14@gmail.com>
2021-08-28 11:18:15 -0300

    Login working :)

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,10 +1,59 @@
 const express = require("express");
 const router = express.Router();
 let User = require("../models/userModel");
+const session = require("express-session");
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
 
-const urlencodedParser = express.urlencoded({ extended: true });
+//Middlewares
+router.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+router.use(express.urlencoded({ extended: true }));
 
-router.post("/user/create", urlencodedParser, async (req, res) => {
+//Passport.js
+router.use(passport.initialize());
+router.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+});
+
+passport.use(
+    new localStrategy((username, password, done) => {
+        User.findOne({ username: username }, (err, user) => {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false, { message: "User does not exist" });
+            }
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                if (res === false) {
+                    return done(null, false, { message: "Incorrect password" });
+                }
+
+                return done(null, user);
+            });
+        });
+    })
+);
+
+router.post("/user/create", async (req, res) => {
     try {
         const hashedPass = await bcrypt.hash(req.body.password, 10);
 
@@ -21,17 +70,35 @@ router.post("/user/create", urlencodedParser, async (req, res) => {
     }
 });
 
-router.get("/", (req, res) => {
+const isLoggedIn = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+};
+
+const isLoggedOut = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/");
+};
+
+router.get("/", isLoggedIn, (req, res) => {
     res.render("index");
 });
 
-router.get("/login", (req, res) => {
+router.get("/login", isLoggedOut, (req, res) => {
     res.render("login");
 });
 
-router.post("/user/auth", (req, res) => {
-    res.send("bro");
-});
+router.post(
+    "/user/auth",
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/login?error=true",
+    })
+);
 
 router.get("/login", (req, res) => {
     res.render("login");
